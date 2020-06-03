63cff4d0abcb55ddf5a1e3c8c4dcef4f9b2df20e
Rohan Singh <rohansingh9001>
2020-06-03 16:51:41 +0530

    Added data spider

---
+++ scrapers/data/data/spiders/user_anime_list.py
@@ -0,0 +1,21 @@
+import scrapy
+
+with open("users.txt", "r", encoding="utf-8") as file:
+    users = file.readlines()
+
+class QuotesSpider(scrapy.Spider):
+    name = "user-anime-list"
+
+    def start_requests(self):
+        urls = [
+            f"https://myanimelist.net/animelist/{user}" for user in users
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-1]
+        filename = '%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
