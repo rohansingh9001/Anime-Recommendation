f5499ebca29ede777e3c358a9d8fea665b3c990d
Rohan Singh <rohansingh9001>
2020-06-03 11:38:43 +0530

    Added and parsed list of all anime

---
+++ scrapers/anime/scrape_anime.py
@@ -0,0 +1,27 @@
+from html.parser import HTMLParser
+import re
+
+anime = []
+
+class ParseAnimeXML(HTMLParser):
+    def handle_data(self, data):
+        if data[:30] == "https://myanimelist.net/anime/":
+            parts = data.split("/")
+            ID, NAME = parts[4], parts[5]
+            anime.append(f"{ID},{NAME}\n")
+            
+
+with open("anime.xml", "r", encoding="utf-8") as file:
+    xml = file.read()
+    parser = ParseAnimeXML()
+    parser.feed(xml)
+
+with open("anime.csv", "w", encoding="utf-8") as file:
+    file.writelines(anime)
+
+
+
+
+
+
+
