201bb80f605531226307158a7402584d4cc4da8e
TarunTomar122 <Tarun@2001>
2020-06-15 05:35:55 +0530

    added notebook

--- scrapers/user/user/spiders/scrape_user.py
+++ scrapers/user/user/spiders/scrape_user.py
@@ -5,9 +5,10 @@ import concurrent.futures
 import math
 
 # Set the number of concurrent spiders you want to crawl
-THREADS = 4
+THREADS = 10
 
 # # Just random combos I thought will not give many results for testing purpose
+# Yes i guessed since You are a hoe!
 # keywords = ['xqc', 'ycv', 'hcz', 'hzc', 'pfg', 'wqv']
 
 keywords = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
@@ -18,8 +19,8 @@ total_crawlers = len(keywords)
 # Function to run the spider on
 def char_spider(i):
     global at_keyword
-    os.system(f"python user.py {keywords[at_keyword + i]}")
-    return f"Spider Done for {keywords[at_keyword + i]}"
+    os.system(f'python3 user.py {keywords[at_keyword + i]}')
+    return f'Spider Done for {keywords[at_keyword + i]}'
 
 # Multiple spiders
 for _ in range(math.ceil(total_crawlers/THREADS)):
--- scrapers/user/user/spiders/user.py
+++ scrapers/user/user/spiders/user.py
@@ -1,16 +1,20 @@
 import scrapy
 from scrapy.http import Request
-from scrapy.crawler import CrawlerProcess
-import sys
 
-usernames = set([])
+from itertools import product
+from string import ascii_lowercase
+keywords = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
+index = 0
+
+import time
+
+userSet = set([])
+i = 1
 
 class ProfileSpider(scrapy.Spider):
-    name = "user"
+    name = "profile"
 
-    def __init__(self, chars=sys.argv[1], *args, **kwargs):
-        super(ProfileSpider, self).__init__(*args, **kwargs)
-        self.start_urls = [f'https://myanimelist.net/users.php?q={chars}&show=1']
+    start_urls = ['https://myanimelist.net/users.php?q=aaa&show=1']
 
     def start_requests(self):
         for url in self.start_urls:
@@ -23,26 +27,47 @@ class ProfileSpider(scrapy.Spider):
         temp = '='.join(temp)
         self.start_urls[0] = temp
 
+    def next_name_page(self):
+        global index, keywords
+        index += 1
+        new_name = keywords[index]
+        temp = self.start_urls[0]
+        temp = temp.split('=')
+        temp[1] = new_name+'&show'
+        temp[-1] = str(1)
+        temp = '='.join(temp)
+        self.start_urls[0] = temp
+
     def parse(self, response):
-        global usernames
+
+        global i, index
+
         self.logger.info('Parse function called on {}'.format(response.url))
 
-        if response.status != 200:
-            pass
-        else:
-            users = response.xpath("//td[@class='borderClass']//div//a")
-            for user in users:
-                username = user.css('a::attr(href)').getall()[0]
-                usernames.add(username)
-            self.next_page()
+        if response.status == 404:
+
+            self.next_name_page()
+
             yield scrapy.Request(self.start_urls[0], callback=self.parse)
 
+        if response.status == 403:
+            print("Index", index)
+        else:
 
-process = CrawlerProcess()
-process.crawl(ProfileSpider)
-process.start()
+            self.next_page()
 
-with open(f"./crawlers/{sys.argv[1]}.txt", "a", encoding="utf-8") as file:
-        for user in usernames:
-            file.write(user + '\n')
+            users = response.xpath("//td[@class='borderClass']//div//a")
 
+            for user in users:
+                userName = user.css('a::attr(href)').getall()[0]
+                userSet.add(userName)
+            
+            print("Length of UserSet is",len(userSet))
+
+            if len(userSet)>1000*i:
+                with open(f"./user/spiders/results/userList{1000*i}.txt","a", encoding="utf-8") as file:
+                    for item in list(userSet):
+                        file.write(item + '\n')
+                    i+=1    
+
+            yield scrapy.Request(self.start_urls[0], callback=self.parse)
